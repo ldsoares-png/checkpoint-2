2fa9203870a59680264d5d688a806cd9357e9970
ldsoares-png <ld.soares@uni9.edu.br>
2025-11-10 22:11:28 -0300

    Implement message classes for different types

---
+++ estrutura_mensagem.py
@@ -0,0 +1,71 @@
+class MensagemBase(ABC):
+    def __init__(self, mensagem: str):
+        self._mensagem = mensagem
+
+    @property
+    def mensagem(self) -> str:
+        return self._mensagem
+
+    @abstractmethod
+    def obter_tipo(self) -> str:
+        pass
+
+class MensagemTexto(MensagemBase):
+  
+    def __init__(self, mensagem: str, data_envio: datetime):
+        super().__init__(mensagem)
+        # Encapsulamento
+        self._data_envio = data_envio
+
+    @property
+    def data_envio(self) -> datetime:
+        return self._data_envio
+
+    def obter_tipo(self) -> str:
+        return "Texto"
+
+class MensagemMidia(MensagemBase):
+
+    def __init__(self, mensagem: str, arquivo: str, formato: str):
+        super().__init__(mensagem)
+
+        self._arquivo = arquivo
+        self._formato = formato
+
+    @property
+    def arquivo(self) -> str:
+        return self._arquivo
+
+    @property
+    def formato(self) -> str:
+        return self._formato
+
+class MensagemFoto(MensagemMidia):
+    
+    def __init__(self, mensagem: str, arquivo: str, formato: str):
+        super().__init__(mensagem, arquivo, formato)
+
+    def obter_tipo(self) -> str:
+        return "Foto"
+
+class MensagemArquivo(MensagemMidia):
+    
+    def __init__(self, mensagem: str, arquivo: str, formato: str):
+        super().__init__(mensagem, arquivo, formato)
+
+    def obter_tipo(self) -> str:
+        return "Arquivo"
+
+class MensagemVideo(MensagemMidia):
+    
+    def __init__(self, mensagem: str, arquivo: str, formato: str, duracao_segundos: int):
+        super().__init__(mensagem, arquivo, formato)
+        # Encapsulamento
+        self._duracao_segundos = duracao_segundos
+
+    @property
+    def duracao_segundos(self) -> int:
+        return self._duracao_segundos
+
+    def obter_tipo(self) -> str:
+        return "Vídeo"
